6d9d343c8543cb90a6ed2776d67ad49e0a222a1b
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-05-15 14:04:31 +0000

    Initial Commit

---
+++ ChartViewSwiftUi/ChartViewSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartViewSwiftUiApp.swift
+//  ChartViewSwiftUi
+//
+//  Created by Abdoulaye Aliou SALL on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChartViewSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
